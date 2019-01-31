e25cf19f6d149d9eec85440d8aa99dbe8b208470
RayapinA <rayapin.antoine36@gmail.com>
2019-01-31 17:07:31 +0100

    ApiTokenAuthenticator

---
+++ src/Controller/UserController.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use phpDocumentor\Reflection\Types\Null_;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class UserController extends AbstractFOSRestController
+{
+    private $userRepository;
+    private $em;
+
+    public function  __construct(UserRepository $userRepository, EntityManagerInterface $em)
+    {
+        $this->userRepository = $userRepository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/user", name="user")
+     */
+    public function index()
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    /**
+     * @Rest\Get("/api/user/{email}")
+     */
+    public function getApiUser(User $user){
+        return $this->json($user);
+    }
+
+    /**
+     * @Rest\Get("/api/users")
+     */
+    public function getApiUsers(){
+        $user = $this->userRepository->findAll();
+        return $this->json($user);
+    }
+
+    /**
+     * @Rest\Post("/api/user")
+     * @ParamConverter("user", converter="fos_rest.request_body")
+     */
+    public function postApiUser(User $user, ConstraintViolationListInterface $validationErrors){
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $errors = array();
+        if($validationErrors->count() > 0){
+            foreach ($validationErrors as $constraintViolation){
+                $message = $constraintViolation->getMessage();
+                $propertyPath = $constraintViolation->getPropertyPath();
+
+                $errors[] = ['messsage' => $message, 'propertyPath' => $propertyPath];
+            }
+        }
+
+        if (!empty($errors)){
+            throw new BadRequestHttpException(\json_encode($errors));
+        }
+
+        return $this->json($user);
+
+    }
+
+    /**
+     * @Rest\Patch("/api/users/{id}")
+     */
+    public function patchApiUser(User $user, Request $request, ValidatorInterface $validator){
+
+        $firstname = $request->get('firstname');
+        $lastname = $request->get("lastname");
+        $createdAt = $request->get("createdAt");
+        $email = $request->get('email');
+        $apiKey = $request->get('apiKey');
+
+
+        if (null !== $firstname){
+            $user->setFirstname($firstname);
+        }
+
+        if (null !== $lastname){
+            $user->setLastname($lastname);
+        }
+
+        if (null !== $createdAt){
+            $user->setCreatedAt(\DateTime::createFromFormat('d-m-Y',$createdAt));
+        }
+
+        if (null !== $email){
+            $user->setEmail($email);
+        }
+        if(null !== $apiKey){
+            $user->setApiKey($apiKey);
+        }
+        $validationErrors = $validator->validate($user);
+        if($validationErrors->count() > 0){
+            foreach ($validationErrors as $constraintViolation){
+                $message = $constraintViolation->getMessage();
+                $propertyPath = $constraintViolation->getPropertyPath();
+
+                $errors[] = ['messsage' => $message, 'propertyPath' => $propertyPath];
+            }
+
+        }
+
+        if (!empty($errors)){
+            throw new BadRequestHttpException(\json_encode($errors));
+        }
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+
+
+        return $this->json($user);
+
+    }
+
+    /**
+     * @Rest\Delete("/api/user/{id}")
+     */
+    public function deleteApiUser(User $user){
+
+        $this->em->remove($user);
+        $this->em->flush();
+
+        return $this->json($this->userRepository->findAll());
+    }
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @UniqueEntity("email")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $apiKey;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $CreatedAt;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getApiKey(): ?string
+    {
+        return $this->apiKey;
+    }
+
+    public function setApiKey(string $apiKey): self
+    {
+        $this->apiKey = $apiKey;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->CreatedAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $CreatedAt): self
+    {
+        $this->CreatedAt = $CreatedAt;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+    public function eraseCredentials(){
+
+    }
+    public function getUsername(){
+
+    }
+
+    public function getSalt(){
+
+    }
+
+    public function getPassword(){
+
+    }
+
+    public function getRoles(): ?array
+    {
+        return $array = array();
+        //return $this->roles;
+    }
+}
